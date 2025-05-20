009621f75c1ebe909cefb44845c20fcbf1d6ae74
Developer <developer@vativeapps.tech>
2025-05-20 18:45:12 +0500

    some fixes

--- src/routes/nftRoutes.ts
+++ src/routes/nftRoutes.ts
@@ -2,6 +2,7 @@ import express from "express";
 import multer from "multer";
 import { Auction } from "../models/auction";
 import { uploadToPinata, uploadMetadataToPinata } from "../utils/pinata";
+import { auctionContract } from "../utils/web3";
 
 const router = express.Router();
 const upload = multer({ dest: "uploads/" });
@@ -38,10 +39,14 @@ router.post("/startAuction", async (req, res) => {
             return;
         }
 
+        const strAuc = await auctionContract.startAuction(tokenId, auctionEndTime);
+        await strAuc.wait();
+
+
         const auction = new Auction({ name, description, imageCID, metadataCID, tokenId, auctionEndTime });
 
         auction.save();
-        res.status(200).json({ success: "Success", data: auction });
+        res.status(200).json({ success: "Success", data: auction, strAucHash: strAuc.hash });
 
         return;
 
--- src/utils/web3.ts
+++ src/utils/web3.ts
@@ -1,13 +1,12 @@
 import { ethers } from "ethers";
 import * as dotenv from "dotenv";
 import contractJson from "../../artifacts/contracts/Auction.sol/AuctionContract.json";
-
 dotenv.config();
 
 const provider = new ethers.JsonRpcProvider(process.env.RPC_URL);
 const wallet = new ethers.Wallet(process.env.PRIVATE_KEY!, provider);
 
-export const auctionContract = new ethers.Contract( 
+export const auctionContract = new ethers.Contract(
     process.env.CONTRACT_ADDRESS!,
     contractJson.abi,
     wallet
